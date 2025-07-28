d12d0b79a97b8c615523fedc9faafbecfc0b7eca
GunterSuperPenguin <random.mail21@list.ru>
2025-07-28 18:58:05 +0700

    New Version
    Co-Authored-By: Chucapabra <185498294+Chucapabra@users.noreply.github.com>

---
+++ Windows/Main.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DNDHelper.Windows
+{
+	/// <summary>
+	/// Логика взаимодействия для Main.xaml
+	/// </summary>
+	public partial class Main : Window
+	{
+
+		public Main()
+		{
+			InitializeComponent();
+			
+		}
+
+		private void UrlTusha(object sender, RoutedEventArgs e)
+		{
+			Process.Start(new ProcessStartInfo("https://docs.google.com/document/d/1rUMWdTp645Zy80d09ZMoJ6dmcw9lAP25tqxsdNkftaY/edit?usp=sharing") { UseShellExecute = true });
+		}
+
+		private void Settings_Click(object sender, RoutedEventArgs e)
+		{
+			Settings settings = new Settings();
+			settings.Show();
+		}
+	}
+}
---
+++ Windows/Settings.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ModernWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DNDHelper.Windows
+{
+	/// <summary>
+	/// Логика взаимодействия для Settings.xaml
+	/// </summary>
+	public partial class Settings : Window
+	{
+		public Settings()
+		{
+			InitializeComponent();
+		}
+
+		private void ComboBoxTheme_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			ComboBox comboBox = sender as ComboBox;
+
+			if (comboBox != null)
+			{
+				int selectionIndex = comboBox.SelectedIndex;
+				object selectionItem = ComboBoxTheme.SelectedItem;
+				switch (selectionIndex)
+				{
+					case 0:
+						ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
+						break;
+					case 1:
+						ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+						break;
+				}
+			}
+        }
+
+		private void AddProfile_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
